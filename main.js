497d655f8068ed473f98c094aae6ff918f46fd2a
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2022-12-10 07:44:58 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let centro = "     Centro     ";
+let izq = "Texto al lado izquierdo     ";
+let derecho = "     Texto al lado derecho"
+
+console.log(izq,centro,derecho); // Texto al lado izquierdo         Centro          Texto al lado derecho
+console.log(izq.length,centro.length,derecho.length); // 28 16 26
+
+let ejm_centro = centro.trim();
+let ejm_izq = izq.trimEnd();
+let ejm_derecho = derecho.trimStart();
+
+console.log(ejm_izq,ejm_centro,ejm_derecho); //Texto al lado derecho Centro Texto al lado izquierdo
+console.log(ejm_izq.length,ejm_centro.length,ejm_derecho.length); // 23 6 21
